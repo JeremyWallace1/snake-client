1debc9ee9d2912c67d49d41ea7f9503a17eab14f
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-23 10:46:44 -0700

    added setupInput and handleUserInput which will handle ctrl-c being sent and return 'exiting the game'

--- play.js
+++ play.js
@@ -1,4 +1,27 @@
 const { connect } = require("./client");
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+  // your code here
+  process.stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      console.log("Exiting the game.");
+      process.exit();
+    }
+    process.stdout.write('.');
+  });
+};
+
 console.log("Connecting ...");
 connect();
+setupInput();
