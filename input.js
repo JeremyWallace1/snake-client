028beb1d372fcc643597d03401ada49f3d862ec3
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-23 13:44:59 -0700

    added conn so able to communicate with server

--- input.js
+++ input.js
@@ -4,7 +4,7 @@ const { stdin } = require("process");
 let connection;
 
 const setupInput = (conn) => {
-  connection = conn;
+  connection = conn; //setupInput() places a reference to conn (from play.js) into the variable connection which is in the global scope of this module.
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -13,26 +13,30 @@ const setupInput = (conn) => {
   return stdin;
 };
 
-const handleUserInput = function (data) {
+const handleUserInput = function (data) { //When data comes in from your keyboard, the stdin event handler can now interact with the server because the scope in the handler now includes both data from the keyboard AND the connection object!
   if (data === '\u0003') {
     console.log("Exiting the game.");
     process.exit();
   }
 
   if (data === '\u0077') {
-    console.log("Pressed w");
+    console.log("Pressed", data);
+    connection.write('up');
   }
   
   if (data === '\u0061') {
-    console.log("Pressed a");
+    // console.log("Pressed a");
+    connection.process(data);
   }
   
   if (data === '\u0073') {
-    console.log("Pressed s");
+    // console.log("Pressed s");
+    connection.process(data);
   }
   
   if (data === '\u0064') {
-    console.log("Pressed d");
+    // console.log("Pressed d");
+    connection.process(data);
   }
 };
 
--- play.js
+++ play.js
@@ -2,5 +2,5 @@ const { connect } = require("./client");
 const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-const objConnect = connect();
-setupInput(objConnect);
+const conn = connect(); //connect() returns an object that can be used to interact with the server
+setupInput(conn); //the object, conn, returned by connect() should be passed into setupInput
