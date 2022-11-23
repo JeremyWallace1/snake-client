e34a6665b9904055ef8223c7c954c90fcb7e5528
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-23 10:40:57 -0700

    played with the move commands being sent to the server

--- client.js
+++ client.js
@@ -20,6 +20,31 @@ const connect = function () {
     conn.write("Name: JTW");
   });
 
+  // conn.on('connect', () => {
+  //   setTimeout(() => {
+  //     conn.write("Move: up");
+  //   }, 50)
+  //   setTimeout(() => {
+  //     conn.write("Move: up");
+  //   }, 100)
+  //   setTimeout(() => {
+  //     conn.write("Move: up");
+  //   }, 150)
+  //   setTimeout(() => {
+  //     conn.write("Move: up");
+  //   }, 200)
+  // });
+
+/* movement commands supported by the game server
+"Move: up" - move up one square (unless facing down)
+"Move: down" - move down one square (unless facing up)
+"Move: left" - move left one square (unless facing right)
+"Move: right" - move left one square (unless facing left)
+*/
+// NOTE: Snakes cannot instantly make a 180 turn by moving in the opposite direction.
+
+
+
   
   return conn;
 };
