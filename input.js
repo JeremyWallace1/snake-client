b72bc2cca782cae5070965db07ebddc54eba8340
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-23 10:51:15 -0700

    moved setupInput and handleUserInput into input.js and exported module for setupInput and require added in client.js

---
+++ input.js
@@ -0,0 +1,22 @@
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+  // your code here
+  process.stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      console.log("Exiting the game.");
+      process.exit();
+    }
+    //process.stdout.write('.');
+  });
+};
+
+module.exports = { setupInput }; // exports the function
